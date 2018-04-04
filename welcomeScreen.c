26c450a3c627fd909781ddcc6b15c3c7afdb748a
Abhisek Juneja <abhisekjuneja@gmail.com>
2018-04-04 16:27:20 +0530

    Create welcomeScreen.c

---
+++ welcomeScreen.c
@@ -0,0 +1,11 @@
+int welcomeScreen();
+    int option;
+    printf("Queenfisher Online Ticket Booking System!\n\n");
+    
+    printf("[1.] Login\n");
+    printf("[2.] Register\n\n");
+    
+    printf("Enter Your Choice: ");
+    scanf("%d", &option);
+    return option;
+}
